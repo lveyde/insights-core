677a1612379a115ba9e07cb05aafef15e9878102
Link Dupont <link@sub-pop.net>
2020-02-10 09:20:18 -0500

    Remove get_legacy_report
    Signed-off-by: Link Dupont <link@sub-pop.net>

--- insights/client/phase/v1.py
+++ insights/client/phase/v1.py
@@ -191,7 +191,6 @@ def post_update(client, config):
             if (not config.disable_schedule and
                get_scheduler(config).set_daily()):
                 logger.info('Automatic scheduling for Insights has been enabled.')
-
         return
     # -------delete everything above this line-------
 
